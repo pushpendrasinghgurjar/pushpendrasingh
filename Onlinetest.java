8a4bb944a4ee1d42bcc890ecd22c355ef02a9d79
pushpendrasinghgurjar <64729519+pushpendrasinghgurjar@users.noreply.github.com>
2020-05-03 15:43:13 +0530

    COMPUTER BASED TEST

---
+++ Onlinetest.java
@@ -0,0 +1,196 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+class Onlinetest extends JFrame implements ActionListener {
+    JLabel l;
+    JRadioButton jb[] = new JRadioButton[5];
+    JButton b1, b2;
+    ButtonGroup bg;
+    int count = 0, current = 0, x = 1, y = 1, now = 0;
+    int m[] = new int[10];
+
+    Onlinetest(String s) {
+        super(s);
+        l = new JLabel();
+        add(l);
+        bg = new ButtonGroup();
+        for (int i = 0; i < 5; i++) {
+            jb[i] = new JRadioButton();
+            add(jb[i]);
+            bg.add(jb[i]);
+        }
+        b1 = new JButton("Next");
+        b2 = new JButton("Bookmark");
+        b1.addActionListener(this);
+        b2.addActionListener(this);
+        add(b1);
+        add(b2);
+        set();
+        l.setBounds(30, 40, 700, 20);
+        jb[0].setBounds(50, 80, 200, 20);
+        jb[1].setBounds(50, 110, 200, 20);
+        jb[2].setBounds(50, 140, 200, 20);
+        jb[3].setBounds(50, 170, 200, 20);
+        b1.setBounds(100, 240, 100, 30);
+        b2.setBounds(270, 240, 100, 30);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+        setLocation(250, 100);
+        setVisible(true);
+        setSize(1200, 800);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == b1) {
+            if (check())
+                count = count + 1;
+            current++;
+            set();
+            if (current == 9) {
+                b1.setEnabled(false);
+                b2.setText("Result");
+            }
+        }
+        if (e.getActionCommand().equals("Bookmark")) {
+            JButton bk = new JButton("Bookmark" + x);
+            bk.setBounds(480, 20 + 30 * x, 100, 30);
+            add(bk);
+            bk.addActionListener(this);
+            m[x] = current;
+            x++;
+            current++;
+            set();
+            if (current == 9)
+                b2.setText("Result");
+            setVisible(false);
+            setVisible(true);
+        }
+        for (int i = 0, y = 1; i < x; i++, y++) {
+            if (e.getActionCommand().equals("Bookmark" + y)) {
+                if (check())
+                    count = count + 1;
+                now = current;
+                current = m[y];
+                set();
+                ((JButton) e.getSource()).setEnabled(false);
+                current = now;
+            }
+        }
+
+        if (e.getActionCommand().equals("Result")) {
+            if (check())
+                count = count + 1;
+            current++;
+            //System.out.println("correct ans="+count);
+            JOptionPane.showMessageDialog(this, "correct ans=" + count);
+            System.exit(0);
+        }
+    }
+
+    void set() {
+        jb[4].setSelected(true);
+        if (current == 0) {
+            l.setText("Que1:To which of the following states, Article 370 if the Indian constitution is related:?");
+            jb[0].setText(" Arunachal Pradesh");
+            jb[1].setText("PUNJAB");
+            jb[2].setText("JAMMU & KASMIR");
+            jb[3].setText("West Bengal");
+        }
+        if (current == 1) {
+            l.setText("Que2: Article 370 is drafted in the part………of the Indian Constitution.?");
+            jb[0].setText("XXI");
+            jb[1].setText("XIX");
+            jb[2].setText("XII");
+            jb[3].setText(" IXX");
+        }
+        if (current == 2) {
+            l.setText("Que3: Who is the national president of congress?");
+            jb[0].setText("RAHUL GANDHI ");
+            jb[1].setText("PRIYANKA GANDHI");
+            jb[2].setText("SONIA GANDHI");
+            jb[3].setText("MANMOHAN SINGH");
+        }
+        if (current == 3) {
+            l.setText("Que4: WHO was working president of BJP?");
+            jb[0].setText("NARENDRA MODI");
+            jb[1].setText("AMIT SHAH");
+            jb[2].setText("J P NADDA");
+            jb[3].setText("SHIVRAJ SINGH CHOHAN");
+        }
+        if (current == 4) {
+            l.setText("Que5: Which group in JECRC Releted to Blood Donation?");
+            jb[0].setText("Zarurat");
+            jb[1].setText("Soch");
+            jb[2].setText("Aashayien");
+            jb[3].setText("none of these");
+        }
+        if (current == 5) {
+            l.setText("Que6: The concept of Goods and Services Tax (GST) is originated in………..?");
+            jb[0].setText("CANADA");
+            jb[1].setText("JAPAN");
+            jb[2].setText("CHAINA");
+            jb[3].setText("RUSIA");
+        }
+        if (current == 6) {
+            l.setText("Que7: Who was the nationl Spokeperson of BJP ");
+            jb[0].setText("Dr.Sambit Patra");
+            jb[1].setText("GOURAV Valabh");
+            jb[2].setText("ABHISHEK MANU");
+            jb[3].setText("NONE of these");
+        }
+        if (current == 7) {
+            l.setText("Que8: who was the Governer  of Rajasthan");
+            jb[0].setText("Satyapal Malik");
+            jb[1].setText("Kalyan Singh");
+            jb[2].setText("Aanndi Ben");
+            jb[3].setText("Kalraj Mishra");
+        }
+        if (current == 8) {
+            l.setText("Que9:Who was the foreign minister of india?");
+            jb[0].setText("Shushama Swaraj");
+            jb[1].setText("ARUN JETAILY");
+            jb[2].setText("RAJANATH SINGH");
+            jb[3].setText("S.JAYASHANKAR");
+        }
+        if (current == 9) {
+            l.setText("Que10: Which  Inheritance java not support ?");
+            jb[0].setText("single");
+            jb[1].setText("multilevel");
+            jb[2].setText("Multiple");
+            jb[3].setText("Inheritancel");
+        }
+        l.setBounds(30, 40, 450, 20);
+        for (int i = 0, j = 0; i <= 90; i += 30, j++)
+            jb[j].setBounds(50, 80 + i, 200, 20);
+    }
+
+    boolean check() {
+        if (current == 0)
+            return (jb[2].isSelected());
+        if (current == 1)
+            return (jb[0].isSelected());
+        if (current == 2)
+            return (jb[2].isSelected());
+        if (current == 3)
+            return (jb[2].isSelected());
+        if (current == 4)
+            return (jb[2].isSelected());
+        if (current == 5)
+            return (jb[0].isSelected());
+        if (current == 6)
+            return (jb[0].isSelected());
+        if (current == 7)
+            return (jb[3].isSelected());
+        if (current == 8)
+            return (jb[3].isSelected());
+        if (current == 9)
+            return (jb[2].isSelected());
+        return false;
+    }
+
+    public static void main(String args[]) {
+        new Onlinetest("Online Test made by Pushpendra Singh ");
+    }
+}
+
